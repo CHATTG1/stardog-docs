d270e4dfa920a3fb0edac25c430af136e75f79ba
Kendall Clark <kendall@clarkparsia.com>
2016-05-18 10:21:15 -0400

    update version and release date in build file

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -3,8 +3,8 @@
 module.exports = function(grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-    var theId = grunt.option("id") || "4.0.5";
-    var theDate = grunt.option("date") || "18 February 2016";
+    var theId = grunt.option("id") || "4.1";
+    var theDate = grunt.option("date") || "18 May 2016";
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
